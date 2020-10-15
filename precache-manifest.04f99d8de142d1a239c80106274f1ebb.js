57109ecb1e57ee3553a81c590fdde1582905c703
Phoebe Wu <Phoebe@elitezone.tech>
2020-10-15 14:38:31 +0800

    Updates

---
+++ precache-manifest.04f99d8de142d1a239c80106274f1ebb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9e137a95bc0ca1b54d0f4e009ee667f",
+    "url": "/aaa/index.html"
+  },
+  {
+    "revision": "a69917a80bab04e8868f",
+    "url": "/aaa/static/css/2.7ff19518.chunk.css"
+  },
+  {
+    "revision": "a35eb5a1086f671195e7",
+    "url": "/aaa/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "a69917a80bab04e8868f",
+    "url": "/aaa/static/js/2.b37d8316.chunk.js"
+  },
+  {
+    "revision": "a35eb5a1086f671195e7",
+    "url": "/aaa/static/js/main.091d7ba5.chunk.js"
+  },
+  {
+    "revision": "4682f09b634cb034b101",
+    "url": "/aaa/static/js/runtime~main.7834b413.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/aaa/static/media/logo.5d5d9eef.svg"
+  }
+]);
